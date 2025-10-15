d0d1caefbdf67fb31a7494548c9706ffb3178090
Ram23471A05DS <jakkularamanjaneyulu0@gmail.com>
2025-10-15 19:47:18 +0530

    i have learned arrays

---
+++ DSA/array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class array
+{
+public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int arr[]=new int[a];
+    for(int i=0;i<a;i++){
+        arr[i]=sc.nextInt();
+
+    }
+    for(int i=0;i<a;i++){
+        System.out.println(arr[1]);
+    }
+
+}
+}
